d856a33337a42518f58becd5f94fe2bb6581838c
Hans Csida <johann.csida@gmail.com>
2016-03-02 16:05:35 +0100

    io message

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,10 @@ var socket = io();
 
 socket.on('connect',function(){
 	console.log('Connected to Server')
+});
+
+socket.on('message',function(message){
+console.log('new message');
+console.log(message.text);
+
 })
